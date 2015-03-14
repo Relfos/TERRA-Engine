afeebc34d6da59601b2a868cc883ffc35511c352
Relfos <relfos@gmail.com>
2015-03-14 08:34:29 +0000

    Added TERRA with VCL sample

---
+++ Samples/Source/Extra/VCL/main.dfm
@@ -0,0 +1,18 @@
+object Form1: TForm1
+  Left = 192
+  Top = 125
+  Width = 606
+  Height = 492
+  Caption = 'TERRA Demo'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ Samples/Source/Extra/VCL/main.pas
@@ -0,0 +1,75 @@
+unit main;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+Uses TERRA_Utils, TERRA_Application, TERRA_VCLClient, TERRA_OS, TERRA_Scene, TERRA_Texture,
+  TERRA_Viewport, TERRA_FileManager, TERRA_SpriteManager, TERRA_PNG,
+  TERRA_GraphicsManager, TERRA_Math, TERRA_Vector2D, TERRA_Color;
+
+{$R *.dfm}
+
+Type
+  MyScene = Class(Scene)
+    Procedure RenderSprites(V:Viewport); Override;
+  End;
+
+Var
+  _Tex:Texture = Nil;
+  _Scene:MyScene;
+
+{ MyScene }
+Procedure MyScene.RenderSprites(V: Viewport);
+Var
+  S:Sprite;
+  Angle:Single;
+Begin
+  // A rotating sprite in the bottom, with Scale = 4x
+  Angle := RAD * ((GetTime() Div 15) Mod 360);
+  S := SpriteManager.Instance.DrawSprite(100, 100, 50, _Tex);
+  S.SetScaleAndRotationRelative(VectorCreate2D(0.5, 0.5), 4.0, Angle);  // Calculate rotation, in degrees, from current time
+End;
+
+Procedure TForm1.FormCreate(Sender: TObject);
+Begin
+  ApplicationStart(VCLWindowClient.Create(Self));
+
+  // Added Asset folder to search path
+  FileManager.Instance.AddPath('assets');
+
+  // Load a Tex
+  _Tex := TextureManager.Instance['ghost'];
+
+  // Create a scene and set it as the current scene
+  _Scene := MyScene.Create;
+  GraphicsManager.Instance.SetScene(_Scene);
+
+  // set background color
+  GraphicsManager.Instance.ActiveViewport.BackgroundColor := ColorGreen;
+End;
+
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  ReleaseObject(_Scene);
+  Application.Instance.Terminate();
+end;
+
+end.
