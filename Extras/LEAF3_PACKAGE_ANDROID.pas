72162d2a8cdcb9063820fcbdadaf02522c783de4
Relfos <relfos@gmail.com>
2015-03-24 11:49:03 +0000

    Added Lazarus packages

---
+++ Extras/LEAF3_PACKAGE_ANDROID.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit LEAF3_PACKAGE_ANDROID;
+
+interface
+
+uses
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('LEAF3_PACKAGE_ANDROID', @Register);
+end.
