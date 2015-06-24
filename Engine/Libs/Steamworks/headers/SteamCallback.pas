b746e711e84992864c684f11b1ff7085b3736fba
Relfos <relfos@gmail.com>
2015-06-24 02:58:15 +0100

    Added support for thiscall callbacks (necessary for interop with Steam)

---
+++ Engine/Libs/Steamworks/headers/SteamCallback.pas
@@ -0,0 +1,122 @@
+Unit SteamCallback;
+
+Interface
+
+Uses SteamAPI;
+
+Type
+  PSteam_UserStatsReceived = ^Steam_UserStatsReceived;
+  Steam_UserStatsReceived = Packed Record
+		GameID:SteamGameID;		      // Game these stats are for
+		Result:SteamResult;	  // Success / error fetching the stats
+		steamID:SteamID;	// The user for whom the stats are retrieved for
+	End;
+
+  PCCallbackBaseVTable = ^TCCallbackBaseVTable;
+  TCCallbackBaseVTable = Record
+    Run,
+    Run_2,
+    GetCallbackSizeBytes: pointer;
+  End;
+
+  SteamCallbackDispatcher = Class;
+
+  PCCallbackInt = ^TCCallbackInt;
+  TCCallbackInt = Record
+    vtable:Pointer;
+    _nCallbackFlags: byte;
+    _iCallback: integer;
+    _Dispatcher:SteamCallbackDispatcher;
+  End;
+
+  SteamCallbackDelegate = Procedure(answer:Pointer) of Object;
+
+  SteamCallbackDispatcher = Class
+    Private
+      _SteamInterface: TCCallbackInt;
+      _Callback: SteamCallbackDelegate;
+      _PropSize: Integer;
+      _CallbackID:Integer;
+
+    Public
+      Constructor Create(iCallback:integer; CallbackProc:SteamCallbackDelegate; _propsize:integer); Reintroduce;
+      Destructor Destroy;
+  End;
+
+
+Implementation
+Uses TERRA_Error;
+
+{ TSteamCallback }
+
+Var
+  MyCallbackVTable: TCCallbackBaseVTable;
+
+Procedure MySteamCallback_Run(pSelf: PCCallbackInt; pvParam: Pointer); Pascal;
+Begin
+  pSelf._Dispatcher._Callback(Pointer(pvParam));
+End;
+
+Procedure MySteamCallback_Run_2(pvParam: PCCallbackInt); Pascal;
+Var
+  myself: PCCallbackInt;
+Begin
+Asm
+  mov myself, ECX;
+End;
+  Myself._Dispatcher._Callback(Pointer(pvParam));
+End;
+
+Function MySteamCallback_GetCallbackSizeBytes:Integer; Pascal;
+Var
+  myself: PCCallbackInt;
+Begin
+  Asm
+    mov myself, ECX;
+  End;
+  Result := Myself._Dispatcher._PropSize;
+End;
+
+(* 64 bits
+procedure MySteamCallback_Run(pSelf: PCCallbackInt; pvParam: Pointer); pascal;
+begin
+
+end;
+
+procedure MySteamCallback_Run_2(myself: PCCallbackInt; pvParam: PCCallbackInt); pascal;
+begin
+TSteamCallbackBase(myself.delphiclass).FProcedure(pvParam);
+end;
+
+function MySteamCallback_GetCallbackSizeBytes(myself: PCCallbackInt):
+Integer; pascal;
+begin
+result := TSteamCallbackBase(myself.delphiclass).FPropSize;
+end;
+
+*)
+
+Constructor SteamCallbackDispatcher.Create(iCallback:integer; callbackProc:SteamCallbackDelegate; _propsize: integer);
+Begin
+  _CallbackID := iCallback;
+  _Callback := callbackProc;
+  _PropSize := _propsize;
+
+  _SteamInterface.vtable := @MyCallbackVTable;
+  _SteamInterface._nCallbackFlags := 0;
+  _SteamInterface._iCallback := 0;
+  _SteamInterface._Dispatcher := Self;
+
+  SteamAPI_RegisterCallback(@_SteamInterface, _CallbackID);
+End;
+
+Destructor SteamCallbackDispatcher.Destroy;
+Begin
+  SteamAPI_UnregisterCallback(@_SteamInterface);
+End;
+
+Initialization
+  MyCallbackVTable.Run := @MySteamCallback_Run;
+  MyCallbackVTable.Run_2 := @MySteamCallback_Run_2;
+  MyCallbackVTable.GetCallbackSizeBytes := @MySteamCallback_GetCallbackSizeBytes;
+End.
